d115a2b691cb2f7fb40e7e8fe9962d9657c62fb7
Pysched <48335713+Pysched@users.noreply.github.com>
2023-11-04 22:06:47 +0000

    Updated the pom to include missing dependencey Updated DarrensPetitionsApplication to resolve to port 9090 and also other imports

--- src/main/java/com/example/DarrensPetitions/DarrensPetitionsApplication.java
+++ src/main/java/com/example/DarrensPetitions/DarrensPetitionsApplication.java
@@ -1,13 +1,18 @@
 package com.example.DarrensPetitions;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DarrensPetitionsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DarrensPetitionsApplication.class, args);
+		SpringApplication app = new SpringApplication(DarrensPetitionsApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "9090"));
+		app.run(args);
 	}
-
 }
