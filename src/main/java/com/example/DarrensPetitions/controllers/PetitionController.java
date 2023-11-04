e38857c7898528b5e5b9803d8723f012e9dd663f
Pysched <48335713+Pysched@users.noreply.github.com>
2023-11-04 23:09:44 +0000

    Create.html - added a container with a card housing a form with an input for petition title and submit button. PetitionController - created a processCreate block of code with a post request method,  to deal with the input values from the create form. These values are added to the List petitions. Updated the fragments.html to modify the navbar styling and piping in the branding location. index.html added a thymeleaf for each method to list out the values submitted on the create page, to be developed further.

--- src/main/java/com/example/DarrensPetitions/controllers/PetitionController.java
+++ src/main/java/com/example/DarrensPetitions/controllers/PetitionController.java
@@ -3,21 +3,33 @@ package com.example.DarrensPetitions.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.*;
 
 @Controller
 public class PetitionController {
+    static List<String> petitions = new ArrayList<>();
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("title", "Darren's Petitions");
         model.addAttribute("pageTitle", "Index Page");
+        model.addAttribute("petitions", petitions);
         return "index";
     }
 
-    @RequestMapping("/create")
+    @RequestMapping(value="create", method = RequestMethod.GET)
     public String create(Model model){
         model.addAttribute("title", "Create a Petition");
         model.addAttribute("pageTitle", "Create Page");
         return "create";
+    }
+
+    @RequestMapping(value="create", method = RequestMethod.POST)
+    public String processCreate(@RequestParam String petitionTitle){
+       petitions.add(petitionTitle);
+        return "redirect:";
     }
 
     @RequestMapping("/search")
